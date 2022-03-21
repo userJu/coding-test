c0ffc53f927c5251e0f940c32c82c90e7691337b
JuhyunLim <boochickenood@gmail.com>
2022-03-21 21:29:14 +0900

    [Silver IV] Title: 한수, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,10 @@
+import sys
+n = int(sys.stdin.readline())
+cnt = 0
+for i in range(1,n+1):
+    arr = list(map(int, str(i)))
+    if i < 100:
+        cnt+=1
+    elif arr[0]-arr[1]==arr[1]-arr[2]:
+        cnt+=1
+print(cnt)
