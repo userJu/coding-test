2356af12b222345ccbd50d122b9990baa9247f8e
JuhyunLim <boochickenood@gmail.com>
2022-03-29 23:17:54 +0900

    [Silver V] Title: 소수, Time: 608 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -3,13 +3,15 @@ n = int(input())
 arr=[]
 for i in range(m,n+1):
     noPrime = 0
-    if i>1:
+    if i>2:
         for j in range(2, i):
             if i % j ==0:
                 noPrime +=1
                 break
         if noPrime==0:
             arr.append(i)
+    elif i == 2:
+        arr.append(i)
 
 if len(arr)>0:
     print(sum(arr))
