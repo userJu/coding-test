7653e498960ca70aca454c483fe9539a509fe8c9
JuhyunLim <boochickenood@gmail.com>
2022-04-02 21:47:37 +0900

    [Silver III] Title: 스택 수열, Time: 4196 ms, Memory: 34180 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,24 @@
+n = int(input())
+stack=[] 
+res=''
+cur = 1 
+for _ in range(n):
+    x = int(input())
+    while cur < x :
+        stack.append(cur)
+        cur+=1
+        res+='+'
+    if cur == x :
+        res+='+-'
+        cur+=1
+    elif cur > x:
+        if stack.pop() == x:
+            res+='-'
+        else:
+            print('NO')
+            cur = 0
+            break
+if cur != 0:
+    print(*res, sep="\n")
+
+        
