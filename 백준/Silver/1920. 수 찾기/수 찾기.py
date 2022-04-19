695fd48c9751ce72b665814163673fd6855a5081
JuhyunLim <boochickenood@gmail.com>
2022-04-19 20:11:18 +0900

    [Silver IV] Title: 수 찾기, Time: 192 ms, Memory: 50068 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,25 +1,8 @@
 import sys
 n = int(input())
-narr = sorted(list(map(int, sys.stdin.readline().split())))
+narr = set(list(map(int, sys.stdin.readline().split())))
 m = int(input())
 marr = list(map(int, sys.stdin.readline().split()))
 
-def find(x):
-    lt = 0
-    rt = n-1
-    while lt <=rt:
-        mid = (lt + rt)//2
-        if narr[mid] == x:
-            return True
-        if x < narr[mid]:
-            rt = mid-1
-        elif x > narr[mid]:
-            lt = mid+1
-
 for x in marr:
-    if find(x) :
-        print(1)
-    else:
-        print(0)
-
-    
+    print(1) if x in narr else print(0)
