3325b59d247bde1f963ce9b810cee47fae03ff4f
JuhyunLim <boochickenood@gmail.com>
2022-04-24 23:36:57 +0900

    [Gold III] Title: 오등큰수, Time: 1912 ms, Memory: 149424 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.py
+++ 백준/Gold/17299. 오등큰수/오등큰수.py
@@ -1,16 +1,14 @@
 import sys
-n = int(input())
-arr = list(map(int, sys.stdin.readline().split()))
-# F에 수열에 등장하는 원소들의 횟수를 모아놓는다(dict=> 값 : 위치 구조로 만든다)
-# stack을 만들어 하나하나 검사한다
-# 결과를 뽑는 배열을 -1로 arr길이만큼 초기화해놓는다
-F = dict()
+input = sys.stdin.readline
+N = int(input())
+arr = list(map(int,input().split()))
+F={}
 stack=[]
-res = [-1]*n
+res=[-1]*N
 for x in arr:
-    F[x]= F.get(x,0)+1 # 원소 : 원소의 수
-for i in range(n):
-    while stack and F[arr[stack[-1]]] < F[arr[i]]:
-        res[stack.pop()] = arr[i]
+    F[x] = F.get(x,0)+1
+for i in range(N):
+    while stack and F[arr[i]] > F[arr[stack[-1]]]:
+        res[stack.pop()]= arr[i]
     stack.append(i)
 print(*res)
