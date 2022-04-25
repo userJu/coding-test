47c2b7b6593de067f5ecbc1e1e6258755d7ec455
JuhyunLim <boochickenood@gmail.com>
2022-04-25 23:12:13 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 4624 ms, Memory: 38652 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,17 +1,18 @@
-arr=[0]*1000005
-for i in range(3,int(1000000**(1/2))+1):
-    if arr[i]==0:
-        for x in range(2*i,1000001,i):
-            arr[x]=1
-while True:
+prime = [0]*1000002
+for i in range(2,1000001):
+    if prime[i] == 0:
+        for j in range(2*i,1000001,i):
+            prime[j] =1
+        
+while True :
     n = int(input())
     if n == 0 :
         break
     else:
-        for i in range(3,n,2):
-            if arr[i]==0 and arr[n-i]==0:
+        for i in range(2,n+1):
+            if prime[i] == 0 and prime[n-i] == 0:
                 print(f'{n} = {i} + {n-i}')
                 break
-        else: print("Goldbach's conjecture is wrong.")
-        
-        
+
+
+
