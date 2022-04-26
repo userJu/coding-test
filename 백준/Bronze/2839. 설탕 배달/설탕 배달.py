2adc5079e8e1208677559d15f1e91963b9aedcde
JuhyunLim <boochickenood@gmail.com>
2022-04-27 08:35:39 +0900

    [Bronze I] Title: 설탕 배달, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2839. 설탕 배달/설탕 배달.py
+++ 백준/Bronze/2839. 설탕 배달/설탕 배달.py
@@ -1,16 +1,16 @@
-# 정확히 N 배달
-# 봉지는 3과 5
-# 더 적은 개수의 봉지 가져가기
 N = int(input())
-cnt=0
+cnt =0 
 while N >=0:
     if N % 5 == 0:
-        cnt+=int(N//5)
+        cnt+=(N//5)
+        N -= (N//5)*5
+    else:
+        N -= 3
+        cnt+=1
+    if N == 0 :
         print(cnt)
         break
-    N-=3
-    cnt+=1
-    
-else:
+if N < 0 :
     print(-1)
-            
+
+        
