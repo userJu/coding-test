749c1ae97aeb17d270d978729cf0b0a944a4786e
JuhyunLim <boochickenood@gmail.com>
2022-04-27 08:46:54 +0900

    [Silver III] Title: ATM, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,11 +1,13 @@
-# 필요한 시간의 최솟값
-# 사람의 수 <= 1000 => 2승을 해도 된다
+# n이 1000까지
+# p또한 1000까지
 import sys
 input = sys.stdin.readline
 N = int(input())
-arr = list(map(int,input().split())) # 3 1 4 3 2
-arr.sort() # 1 2 3 4 5
-sum = 0
-for i in range(N,0,-1):
-    sum +=(arr[N-i]* i)
-print(sum)
+arr = list(map(int,input().split()))
+arr.sort()
+
+res = 0
+for idx, x in enumerate(arr):
+    res+=x*(N-idx)
+print(res)
+
