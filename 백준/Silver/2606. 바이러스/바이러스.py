72bdaa42369ddb25dd7cddb475067b9d2de0859b
JuhyunLim <boochickenood@gmail.com>
2022-04-28 08:18:53 +0900

    [Silver III] Title: 바이러스, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,29 @@
+# DFS
+
 C = int(input())
 N = int(input())
 
-board=[[0]*(C+1) for _ in range(C+1)]
+graph = [[0]*(C+1) for _ in range(C+1)]
+
 for _ in range(N):
     a,b = map(int,input().split())
-    board[a][b] = board[b][a] = 1
+    graph[a][b] = graph[b][a] = 1
 
 visited = []
 cnt = 0
 
-def DFS(cur):
+def DFS(now):
     global cnt
-    visited.append(cur)
+    visited.append(now)
     for i in range(C+1):
-        if board[cur][i] ==1 and i not in visited:
+        if graph[now][i] == 1 and i not in visited:
             cnt+=1
             DFS(i)
     else:
         return
 
 DFS(1)
-print(cnt)    
+print(cnt)
+    
+    
+    
