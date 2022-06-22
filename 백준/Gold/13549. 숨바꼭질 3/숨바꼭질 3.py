eddba906e0f50c6b1429b25591b08c55cc5c35e8
JuhyunLim <boochickenood@gmail.com>
2022-06-22 22:24:10 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 280 ms, Memory: 36764 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,23 +1,20 @@
 import sys
-from collections import deque
+import heapq
 input = sys.stdin.readline
 N,K = map(int,input().split())
 INF = 2147000000
 
-def BFS(N,K):
-    dq = deque([])
-    dq.append((0,N))
-    ch_board = [INF] * (100001)
-    ch_board[N] = 0
-    while dq:
-        val, node = dq.popleft()
-        for x in [(1,node+1),(1,node-1),(0,node*2)]:
-            if 0<=x[1]<100001:
-                if ch_board[x[1]] > val+x[0]:
-                    ch_board[x[1]] = val+x[0]
-                    dq.append((ch_board[x[1]],x[1]))
-    return ch_board[K]
-
-print(BFS(N,K))
-                    
+def dijkstra(N,K):
+    dist = [INF]*(100001)
+    dist[N] =0 
+    hq = []
+    heapq.heappush(hq,(0,N))
+    while hq:
+        w,n = heapq.heappop(hq)
+        for nx in [(n+1,1),(n-1,1),(n*2,0)]:
+            if 0<=nx[0]<100001 and dist[nx[0]] > w + nx[1]:
+                dist[nx[0]] = w + nx[1]
+                heapq.heappush(hq,(dist[nx[0]],nx[0]))
+    print(dist[K])
+dijkstra(N,K)
     
