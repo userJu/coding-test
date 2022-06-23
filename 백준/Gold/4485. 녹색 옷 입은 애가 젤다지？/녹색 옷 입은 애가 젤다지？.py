5d260ce03e8538c8efaf8ac1c1a82ebfebace958
JuhyunLim <boochickenood@gmail.com>
2022-06-23 09:52:47 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 160 ms, Memory: 32908 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -10,22 +10,24 @@ cnt = 0
 while N!=0:
     cnt+=1
     board = [list(map(int,input().split())) for _ in range(N)]
-    ch_board = [[INF]*N for _ in range(N)]
+    ch_board = [[-1]*N for _ in range(N)]
 
     hq = []
     heapq.heappush(hq,(board[0][0],0,0))
-    ch_board[0][0] = board[0][0]
+    ch_board[0][0] = 0
     
     while hq:
         val,x,y = heapq.heappop(hq)
+        if x == N-1 and y == N-1:
+            print(f'Problem {cnt}: {val}')
+            break
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx < N and 0<=ny<N and ch_board[nx][ny] == INF:
-                if ch_board[nx][ny] > val+board[nx][ny]:
-                    ch_board[nx][ny] = val+board[nx][ny]
-                heapq.heappush(hq,(ch_board[nx][ny],nx,ny))
-    print(f'Problem {cnt}: {ch_board[N-1][N-1]}')
+            if 0<=nx < N and 0<=ny<N and ch_board[nx][ny] == -1:
+                ch_board[nx][ny] = 0
+                heapq.heappush(hq,(val+board[nx][ny],nx,ny))
+
     N = int(input())
 
         
