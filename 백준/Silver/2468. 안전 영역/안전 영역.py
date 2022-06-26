32a5ed05723a46f9ed1f4328029a2042aea2d6bb
JuhyunLim <boochickenood@gmail.com>
2022-06-26 20:51:39 +0900

    [Silver I] Title: 안전 영역, Time: 1496 ms, Memory: 32476 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,36 +1,40 @@
+# 많은 비가 내렸을 때 물에 잠기지 않는 
+# 안전한 영역이 최대로 몇 개가 만들어 지는 지를 조사하려고 한다.
+# 장마철에 물에 잠기지 않는 안전한 영역의 최대 개수
+
 import sys
 from collections import deque
 input = sys.stdin.readline
 N = int(input())
 board = [list(map(int,input().split())) for _ in range(N)]
 
-dx=[-1,0,1,0]
-dy=[0,-1,0,1]
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
 
 def BFS(x,y,k):
     dq = deque([])
     dq.append((x,y))
-    new_board[x][y] = -1
+    ch_board[x][y] = 1
     while dq:
         xx,yy = dq.popleft()
         for i in range(4):
-            nx = xx+dx[i]
-            ny = yy+dy[i]
-            if 0<=nx<N and 0<=ny<N and board[nx][ny] > k and new_board[nx][ny] == 0:
-                new_board[nx][ny] = -1
+            nx = xx + dx[i]
+            ny = yy + dy[i]
+            if 0<=nx<N and 0<=ny<N and ch_board[nx][ny]== 0 and board[nx][ny]>k:
+                ch_board[nx][ny] = 1
                 dq.append((nx,ny))
+                
 
-res = -1
+res = 0
 for k in range(101):
+    ch_board = [[0]*N for _ in range(N)]
     cnt = 0
-    new_board = [[0]*N for _ in range(N)]
     for i in range(N):
         for j in range(N):
-            if board[i][j] >k and new_board[i][j]==0:
+            if board[i][j] >k and ch_board[i][j] == 0:
                 BFS(i,j,k)
                 cnt+=1
-    if cnt == 0 :
+    if cnt == 0:
         break
-    res = max(res,cnt)
+    res= max(res,cnt)
 print(res)
-                
