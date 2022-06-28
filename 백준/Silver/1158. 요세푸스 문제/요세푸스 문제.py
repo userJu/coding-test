d231eda3b318480af608e0af658114f2a159c247
JuhyunLim <boochickenood@gmail.com>
2022-06-28 22:44:22 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 92 ms, Memory: 32416 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,15 +1,11 @@
+import sys
 from collections import deque
-n,k = map(int, input().split())
-arr = deque(list(x for x in range(1,n+1)))
-cnt = 0
-res=[]
+input = sys.stdin.readline
+N,K = map(int,input().split())
+arr = deque([i for i in range(1,N+1)])
+res = []
 while arr:
-    cnt+=1
-    if cnt % k == 0 :
-        res.append(arr.popleft())
-    else:
-        arr.append(arr.popleft())
-print('<',', '.join(str(x) for x in res),'>',sep="")
-        
-        
-    
+    arr.rotate((K-1)*-1)
+    res.append(arr.popleft())
+print(f'<{", ".join(str(x) for x in res)}>')
+
