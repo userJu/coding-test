f108e943c77941ec49060f91fdeedabe136c4717
JuhyunLim <boochickenood@gmail.com>
2022-06-28 10:56:11 +0900

    [Silver II] Title: 베르트랑 공준, Time: 648 ms, Memory: 32768 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,27 +1,19 @@
-def isPrime(x):
-    if x ==1:
-        return False
-    for j in range(2,int(x**0.5)+1):
-        if x % j == 0 :
-            return False
-    return True
+import sys
+input = sys.stdin.readline
+N = int(input())
 
-limit = list(range(2,246912))
-res=[]
-for x in limit:
-    if isPrime(x):
-        res.append(x)
-        
-while True:
-    cnt=0
-    n = int(input())
-    if n == 0 :
-        break
-    for y in res:
-        if n< y <=2*n:
+prime = [0]*((123456*2)+1)
+prime[0] = -1
+prime[1] = -1
+for i in range(2,(int(123456*2**0.5)+1)):
+    if prime[i] == 0:
+        for j in range(i*2, (123456*2)+1, i):
+            prime[j] = -1
+
+while N :
+    cnt = 0
+    for i in range(N+1,2*N+1):
+        if prime[i] == 0:
             cnt+=1
     print(cnt)
-    
-        
-        
-    
+    N = int(input())
