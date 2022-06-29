3b06cf3f9a42d7db7ba4bf0beac6b3d1f43f3fcb
JuhyunLim <boochickenood@gmail.com>
2022-06-30 07:54:37 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,18 +1,17 @@
+"""
+dp
+"""
 import sys
 input = sys.stdin.readline
-
 T = int(input())
-
-dy=[0,1,2,4]
-def dynamic(n):
-    if n<len(dy):
-        return
-    if n>3 and n >=len(dy):
-        for i in range(len(dy),n+1):
-            dy.append(dy[i-1]+dy[i-2]+dy[i-3])
-        return
-
+dp = [0]*12
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
 for _ in range(T):
-    n = int(input())
-    dynamic(n)
-    print(dy[n])
+    N = int(input())
+    for i in range(4,N+1):
+        dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+    print(dp[N])
+        
+    
