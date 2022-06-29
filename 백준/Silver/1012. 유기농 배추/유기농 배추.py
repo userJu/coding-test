566fa6ac5a090ddda3fc18ec0e872ab7cf93d0b6
JuhyunLim <boochickenood@gmail.com>
2022-06-29 22:25:58 +0900

    [Silver II] Title: 유기농 배추, Time: 100 ms, Memory: 32436 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,41 +1,37 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-
 T = int(input())
+
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+
+def BFS(x,y):
+    dq = deque([])
+    dq.append((x,y))
+    board[x][y] = -1
+    while dq:
+        xx,yy = dq.popleft()
+        for i in range(4):
+            nx = xx + dx[i]
+            ny = yy + dy[i]
+            if 0<=nx<N and 0<=ny<M and board[nx][ny] == 1:
+                board[nx][ny] = -1
+                dq.append((nx,ny))
+            
+    
 
 for _ in range(T):
     M,N,K = map(int,input().split())
-
-    board =[[0]*M for _ in range(N)] # 저장도 여기 => 0으로
+    board = [[0]*M for _ in range(N)]
     for _ in range(K):
-        X,Y = map(int,input().split())
-        board[Y][X] = 1
-
-    dx = [-1,0,1,0]
-    dy = [0,-1,0,1]
-
-
-    def BFS(q,w):
-        dq = deque()
-        dq.append((q,w)) # 좌표로 돌기
-
-        while dq:
-            p = dq.popleft()
-
-            for k in range(4):
-                x = p[0]+dx[k]
-                y = p[1]+dy[k]
-                if 0<=x<N and 0<=y<M and board[x][y] == 1:
-                    board[x][y] = 0
-                    dq.append([x,y])
-        return 1
-
-    res = 0
+        x,y = map(int,input().split())
+        board[y][x] = 1
+        
+    cnt = 0
     for i in range(N):
         for j in range(M):
             if board[i][j] == 1:
-                res+=BFS(i,j)
-
-    print(res)
-    
+                BFS(i,j)
+                cnt+=1
+    print(cnt)
