69c9d4ea91d9c8b747b935e3fedd433834ea79ef
JuhyunLim <boochickenood@gmail.com>
2022-07-01 12:16:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 3828 ms, Memory: 33668 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,12 +1,10 @@
 import sys
 input = sys.stdin.readline
-
 N,K = map(int,input().split())
-np = [0]*(K+1)
-
+dp = [0]*(K+1)
 for _ in range(N):
     w,v = map(int,input().split())
     for i in range(K,w-1,-1):
-        np[i] = max(np[i-w]+v, np[i])
-        
-print(np[-1])
+        dp[i] = max(dp[i],dp[i-w]+v)
+print(dp[K])
+    
