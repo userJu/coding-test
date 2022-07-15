c81fd12d3ff39c4451c4497b0b65ca3bcd9f3936
JuhyunLim <boochickenood@gmail.com>
2022-07-15 21:31:06 +0900

    [Silver IV] Title: 큐 2, Time: 2248 ms, Memory: 92584 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n = int(input())
+
+dq = deque([])
+for _ in range(n):
+    cmd = list(input().split())
+    if cmd[0] == 'push':
+        dq.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        if dq:
+            print(dq[0])
+            dq.popleft()
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(dq))
+    elif cmd[0] == 'empty':
+        if dq:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'front':
+        if dq:
+            print(dq[0])
+        else:
+            print(-1)
+    elif cmd[0] == 'back':
+        if dq:
+            print(dq[-1])
+        else:
+            print(-1)
+    
