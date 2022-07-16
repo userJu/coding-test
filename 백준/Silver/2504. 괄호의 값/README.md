6a4a31187a1a19db7ed48321a31b0b2ebe6cfd4b
JuhyunLim <boochickenood@gmail.com>
2022-07-16 14:13:45 +0900

    [Silver I] Title: 괄호의 값, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2504. 괄호의 값/README.md
@@ -0,0 +1,12 @@
+# [Silver I] 괄호의 값 - 2504 
+
+[문제 링크](https://www.acmicpc.net/problem/2504) 
+
+### 성능 요약
+
+메모리: 30840 KB, 시간: 80 ms
+
+### 분류
+
+자료 구조(data_structures), 구현(implementation), 재귀(recursion), 스택(stack)
+
---
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+arr = input().rstrip()
+stack = []
+cnt = 1
+res = 0
+for i in range(len(arr)):
+    if arr[i] == '(':
+        stack.append(arr[i])
+        cnt *= 2
+    elif arr[i] == '[':
+        stack.append(arr[i])
+        cnt *= 3
+    elif arr[i] == ')':
+        if stack == [] or stack[-1] == '[':
+            print(0)
+            break
+        else:
+            if arr[i-1] =='(':
+                res+=cnt
+            stack.pop()
+            cnt //= 2
+    elif arr[i] == ']':
+        if stack == [] or stack[-1] == '(':
+            print(0)
+            break
+        else:
+            if arr[i-1] =='[':
+                res+=cnt
+            stack.pop()
+            cnt //= 3
+else:
+    if stack:
+        print(0)
+    else:
+        print(res)
+    
